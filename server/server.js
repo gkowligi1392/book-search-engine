26b0d8a3b132a5a96e2e38d68eb581a604490866
Gokul's Laptop <gkowligi@gmail.com>
2022-01-12 19:21:13 -0800

    removed controllers and changed proxy server

--- server/server.js
+++ server/server.js
@@ -1,13 +1,12 @@
 const express = require("express");
 const { ApolloServer } = require("apollo-server-express");
 const path = require("path");
-const routes = require("./routes");
 
 const { typeDefs, resolvers } = require("./schemas");
 const { authMiddleware } = require("./utils/auth");
 const db = require("./config/connection");
 
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 5000;
 const app = express();
 
 const startServer = async () => {
@@ -31,7 +30,6 @@ if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "../client/build")));
 }
 
-app.use(routes);
 app.get("*", (req, res) => {
   res.sendFile(path.join(__dirname, "../client/build/index.html"));
 });
